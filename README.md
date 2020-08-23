37954a3ce1eab017a5e9ee1ec1dab917aafc1a1f
hannahjorich <hannah.jorich@gmail.com>
2020-08-23 15:25:23 -0600

    added answers to the generate markedown file

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# adflkjadf
-  ## aldkfj
-
-  *[Installation](#installation)
-
-  *[Description](#Description)
+# readme generator
 
   ## Description
-  > lkjadf
+  > generators a readme file
 
-  ![license](https://img.shields.io/badge/license-BSD%203-blue.svg)
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Contributors](#contributors)
+
+  ## Installation
+  Packages required to run this program are: npm
+  
+  ## Usage
+  Examples of how to use this program: na
+
+  ## Tests
+  To test, run the following command: n/a
+
+  ## Contributors
+  hannah jorich
+
+  ## License
+  ![license](https://img.shields.io/badge/license-APACHE%202.0-blue.svg)
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ## ${data.username}
-
-  *[Installation](#installation)
-
-  *[Description](#Description)
 
   ## Description
   > ${data.description}
 
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Contributors](#contributors)
+
+  ## Installation
+  Packages required to run this program are: ${data.installation}
+  
+  ## Usage
+  Examples of how to use this program: ${data.usage}
+
+  ## Tests
+  To test, run the following command: ${data.tests}
+
+  ## Contributors
+  ${data.contributer}
+
+  ## License
   ![license](https://img.shields.io/badge/license-${data.license}-blue.svg)
 
 `;
